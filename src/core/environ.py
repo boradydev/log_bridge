0db0722143a4910260a9faa15e092933d9d56e67
boradydev <alexvlod85@gmail.com>
2026-04-27 16:51:35 +0300

    Refactor `environ`: rename `env_field` to `environ`, add new `environ_get` method for optional environment variable fetching.

--- src/core/environ.py
+++ src/core/environ.py
@@ -1,14 +1,26 @@
 import os
 from collections.abc import Callable
 from dataclasses import field
-from typing import Any, TypeVar
+from typing import Any
 
 
-T = TypeVar("T")
-
-
-def env_field[T](
-    cast_type: Callable[[Any], T],
+def environ[Type](
+    cast_type: Callable[[Any], Type],
     key: str,
-) -> T:
-    return field(default_factory=lambda: cast_type(os.environ[key]))
+) -> Type:
+    def _cast_type() -> Type:
+        return cast_type(os.environ[key])
+    return field(default_factory=_cast_type)
+
+
+def environ_get[Type](
+    cast_type: Callable[[Any], Type],
+    key: str,
+) -> Type | None:
+    def _cast_type() -> Type | None:
+        value = os.environ.get(key)
+        if not value:
+            return None
+        return cast_type(value)
+
+    return field(default_factory=_cast_type)
